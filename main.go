6512b2b5309f45abce756a2db66368a4ef2693fd
Chetna Bhati <chetnabhati343@gmail.com>
2026-09-03 22:54:20 +0530

    Add student model and sample data

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Student struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
 func main() {
-	fmt.Println("Hello, Go!")
+	students := []Student{
+		{ID: 1, Name: "Chetna Bhati", Age: 23, Email: "chetnabhati343@gmail.com"},
+		{ID: 2, Name: "test01", Age: 82, Email: "test01@gmail.com"},
+		{ID: 3, Name: "test02", Age: 41, Email: "test00@gmail.com"},
+	}
+	fmt.Println(students)
 }
